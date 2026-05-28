0366d8e2e9501c5b1d2dcb2276658318c748e604
amitcsenita <amitcse.nita@gmail.com>
2026-05-28 22:27:20 +0530

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.java
@@ -0,0 +1,36 @@
+class TimeMap {
+    private record TimeBasedValue(int timestamp, String value) {}
+    Map<String, List<TimeBasedValue>> store;
+
+    public TimeMap() {
+        store = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+       store.putIfAbsent(key, new ArrayList<>());
+       store.get(key).add(new TimeBasedValue(timestamp, value));
+    }
+    
+    public String get(String key, int timestamp) {
+         if (!store.containsKey(key)) {
+           return "";
+         }
+         int low = 0;
+         int high = store.get(key).size() - 1;         
+         int latestIndex = -1;
+         List<TimeBasedValue> values = store.get(key);
+
+         while (low <= high) {
+            int mid = low + (high - low)/2;
+            int midTimestamp = values.get(mid).timestamp();
+            if (midTimestamp <= timestamp) {
+              latestIndex = Math.max(latestIndex, mid);
+              low = mid + 1;
+            } else {
+                high = mid-1;
+            }
+         }
+         return latestIndex != -1 ? values.get(latestIndex).value() : "";
+        
+    }
+}
