5a6534fa8c3f136a90d1c16ea5143b0ca1d1c36d
amitcsenita <amitcse.nita@gmail.com>
2026-09-07 23:01:02 +0530

    Add: reorder-linked-list - submission-1

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-1.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+// Amit | NeetCode | 2026-09-07 22:59 IST | Attempt 1 | Brute Force
+// TC = O(n), SC = O(n)
+class Solution {
+    public void reorderList(ListNode head) {
+         if (head == null || head.next == null) return;
+         List<ListNode>nodes = new ArrayList<>();
+
+         ListNode cur = head;
+         while (cur != null) {
+            nodes.add(cur);
+            cur = cur.next;
+         }
+
+         int i = 0, j = nodes.size()-1;
+
+         while (i < j) {
+            nodes.get(i).next = nodes.get(j);
+            i++;
+            if (i>=j) break;
+            nodes.get(j).next = nodes.get(i);
+            j--; 
+         }
+
+         nodes.get(i).next = null;
+    }
+}
